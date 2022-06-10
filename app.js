e19f3883a59640aa474e55fc8756d09c80f4f139
Deepak23254 <103886720+Deepak23254@users.noreply.github.com>
2022-06-10 18:01:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-mongoose.connect(mongoString.prod.url);
+mongoose.connect(mongoString.docker.docker_url);
 const database = mongoose.connection;
 
 database.on('error', (error) => {
